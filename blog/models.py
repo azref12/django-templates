084b4a916cd485203521289ae4f849583fe9be24
azmi_refani <arefani13@gmail.com>
2022-11-15 14:33:33 +0700

    fix models in blog

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,6 @@ class post (models.Model) :
     id_post = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     content = models.TextField()
+    
+    def __str__(self) :
+        return "{}".format(self.title)
