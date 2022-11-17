3a2f479a92d6865b6d21e54da02d1ca5a3931312
azmi_refani <arefani13@gmail.com>
2022-11-17 16:31:06 +0700

    add multiple choice in models.py > blog app

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,13 @@
 from django import forms
 
 class BlogForm (forms.Form) :
-    title = forms.CharField()
+    title = forms.CharField(required=True)
+    
+    PILIHAN = {
+        ('category1','Kdrama'),
+        ('category2','Movies'),
+        ('category3','Variety Show'),
+        ('category4',''),
+    }
+    category = forms.ChoiceField(choices=PILIHAN)
+    
